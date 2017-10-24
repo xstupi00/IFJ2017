6c12b13c41c5da79bbec4de06bd37d9961a5f73a
Matúš Liščinský <spse1po@gmail.com>
2017-10-24 22:08:07 +0200

    ungetToken()

--- main.c
+++ main.c
@@ -8,12 +8,22 @@ int main(){
     
     initToken();
     T_Token * token;
-    int i=0;
-    for(i=0; i<80; i++){
+   // int i=0;
+    /*for(i=0; i<80; i++){
         token = getToken();
         printf("%d\t", token->type);
         printf("%s\n", token->str->string);
-    }
+    }*/
+    token = getToken();
+        printf("%d\t", token->type);
+        printf("%s\n", token->str->string);
+    ungetToken();    
+    token = getToken();
+        printf("%d\t", token->type);
+        printf("%s\n", token->str->string);
+    token = getToken();
+        printf("%d\t", token->type);
+        printf("%s\n", token->str->string);
 
     free(token->str->string);
     free(token->str);
