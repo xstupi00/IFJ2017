cb7d5f3de59f7c7a73d692243e6dca6f4056b0d8
vmarcin <marcinvlado@gmail.com>
2017-11-12 12:32:35 +0100

    basic syntax analysis without expression

--- parser.c
+++ parser.c
@@ -6,62 +6,69 @@
 #include "parser.h"
 #include "scanner.h"
 #include "error.h"
+//#include "expr.h"
 
 T_Token *token;
 
-//Declaration of state function
-bool PROG();
-bool DECLARE_FUNCTION();
-bool DEFINE_FUNCTION();
-bool MAIN_FUNCTION();
-bool FUNCTION_ELEMENT();
-bool ELEMENT_LIST();
-bool STATEMENT();
-bool VALUE();
-bool ELSE_STATEMENT();
-bool STAT_LIST();
-bool EXPRESSION();
-bool CALL_ASSIGN();
-bool PARAM_VALUE();
-bool NEXT_PARAM_VALUE();
-bool PARAM_LIST();
-bool NEXT_PARAM();
-bool PARAM();
-bool DATA_TYPE();
+//Declaration of state functions
+bool PROG();	// :)
+bool DECLARE_FUNCTION();	// :)
+bool DEFINE_FUNCTION();		// :)	
+bool MAIN_FUNCTION();		// :)
+bool FUNCTION_ELEMENT();	// :)
+bool ELEMENT_LIST();		// :)
+bool STATEMENT();		//TODO
+bool VALUE();		//TODO
+bool ELSE_STATEMENT();	//TODO
+bool STAT_LIST();		//TODO
+bool EXPRESSION();		//TODO
+bool CALL_ASSIGN();		//TODO
+bool PARAM_VALUE();		//TODO
+bool NEXT_PARAM_VALUE();	//TODO
+bool PARAM_LIST();	// :)
+bool NEXT_PARAM();	// :)
+bool PARAM();	// :)
+bool DATA_TYPE();	// :)	 
 
 
 void debug_p(char *c){
 	fprintf(stdout, "parser: %s\n", c);
 }
 
-//Definition of rule function
+//Definition of rule functions
 bool PROG(){
 	debug_p("enter PROG");
-
+	
 	if(token->type == DECLARE){
-		if(!DECLARE_FUNCTION())
-			return false;
-		token = getToken();
-		if(token->type == EOL){
-			token = getToken();
-			return PROG();
-		}else return false;	
+		if(DECLARE_FUNCTION()){
+			if(token->type == EOL){
+				token = getToken();
+				return PROG();
+			}
+		}	
 	}
 	else if(token->type == T_FUNCTION){
-		if(!DEFINE_FUNCTION())
-			return false;
-		token = getToken();
-		if(token->type == EOL){
-			token = getToken();
-			return PROG();
-		}else return false;
+		if(DEFINE_FUNCTION()){
+			if(token->type == EOL){
+				token = getToken();
+				return PROG();
+			}
+		}
 	}
-	else if(token->type == SCOPE)
-		return MAIN_FUNCTION();
-
-	else if(token->type == EOL)
+	else if(token->type == SCOPE){
+		if(MAIN_FUNCTION()){
+			while(token->type != END_OF_FILE){
+				if(token->type != EOL)
+					return false;
+				token = getToken();
+			}
+			return true;
+		} 
+	}
+	else if(token->type == EOL){
+		token = getToken();
 		return PROG();
-	
+	}
 	else if(token->type == END_OF_FILE)
 		return true;
 	
@@ -69,173 +76,273 @@ bool PROG(){
 }
 
 bool DECLARE_FUNCTION(){
-	debug_p("enter DECLAERE_FUNCTION");
-	if(token->type == DECLARE)
-		token = getToken();
-	else return false;
-	
-	if(token->type == T_FUNCTION)
-		token = getToken();
-	else return false;
-	
-	if(token->type == ID)
-		token = getToken();
-	else return false;
+	debug_p("enter DECLARE_FUNCTION");
 
-	if(token->type == LEFT_R_BRACKET)
+	if(token->type == DECLARE){
 		token = getToken();
-	else return false;
-
-	if(PARAM_LIST()){
-		if(token->type == RIGHT_R_BRACKET)
+		if(token->type == T_FUNCTION){
 			token = getToken();
-		else return false;
-
-		if(token->type == AS){
-			token = getToken();
-			return DATA_TYPE();
-		}else return false;
+			if(token->type == ID){
+				token = getToken();
+				if(token->type == LEFT_R_BRACKET){
+					token = getToken();
+					if(PARAM_LIST()){
+						if(token->type == AS){
+							token = getToken();
+							return DATA_TYPE();
+						}
+					}
+				}
+			}
+		}
 	}
-	else return false;
+	return false;
 }
 
 bool DEFINE_FUNCTION(){
 	debug_p("enter DEFINE_FUNCTION");
-	if(token->type == T_FUNCTION)
-		token = getToken();
-	else return false;
 	
-	if(token->type == ID)
+	if(token->type == T_FUNCTION){
 		token = getToken();
-	else return false;
-
-	if(token->type == LEFT_R_BRACKET)
-		token = getToken();
-	else return false;
-
-	if(PARAM_LIST()){
-		if(token->type == RIGHT_R_BRACKET)
+		if(token->type == ID){
 			token = getToken();
-		else return false;
-
-		if(token->type == AS)
-			token = getToken();
-		else return false;
-		
-		if(DATA_TYPE()){
-			if(token->type == EOL)
+			if(token->type == LEFT_R_BRACKET){
 				token = getToken();
-			else return false;
-
-			if(FUNCTION_ELEMENT()){
-				if(token->type == END)
-					token = getToken();					
-				else return false;
-
-				if(token->type == T_FUNCTION){
-					token = getToken();
-					return true;
-				}else return false;
-
-			}else return false;
-
-		}else return false;
-
-	}else return false;
+				if(PARAM_LIST()){
+					if(token->type == AS){
+						token = getToken();
+						if(DATA_TYPE()){
+							if(token->type == EOL){
+								token = getToken();
+								if(FUNCTION_ELEMENT()){
+									if(token->type == T_FUNCTION){
+										token = getToken();
+										return true;
+									}
+								}
+							}
+						}	
+					}
+				}
+			}
+		}
+	}
+	return false;
 }
 
 bool MAIN_FUNCTION(){
 	debug_p("enter MAIN_FUNCTION");
-	if(token->type == SCOPE)
+
+	if(token->type == SCOPE){
 		token = getToken();
-	else return false;
-
-	if(token->type == EOL)
-		token = getToken();
-	else return false;
-
-	//TODO look at function_element->epsilon rule
-	//where i have to ask for that
-	if(FUNCTION_ELEMENT()){
-		if(token->type == END)
+		if(token->type == EOL){
 			token = getToken();
-		else return false;
-		
-		if(token->type == SCOPE){
-			token = getToken();
-			return true;
-		}else return false;
-
-	}else return false;
+			if(FUNCTION_ELEMENT()){
+				if(token->type == SCOPE){
+					token = getToken();
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 
 bool FUNCTION_ELEMENT(){
 	debug_p("enter FUNCTION_ELEMENT");
+	if(token->type == END){
+		token = getToken();
+		return true;
+	}
+	else if(token->type == DIM){
+		token = getToken();
+		if(token->type == ID){
+			token = getToken();
+			if(token->type == AS){
+				token = getToken();
+				if(DATA_TYPE() && VALUE()){
+					if(token->type == EOL){
+						token = getToken();
+						return FUNCTION_ELEMENT();
+					}
+				}
+			}
+		}
+	}
+	else if(token->type == EOL	 ||	token->type == ID     || token->type == PRINT || 
+			token->type == INPUT || token->type == RETURN || token->type == DO	  ||
+			token->type == IF){
+		return ELEMENT_LIST();
+	}
 	return false;
 }
 
 bool ELEMENT_LIST(){
 	debug_p("enter ELEMENT_LIST");
+	if(	token->type == EOL	 ||	token->type == ID     || token->type == PRINT || 
+		token->type == INPUT || token->type == RETURN || token->type == DO	  ||
+		token->type == IF){
+		return STATEMENT() && FUNCTION_ELEMENT();
+	}
 	return false;
 }
 
 bool STATEMENT(){
 	debug_p("enter STATEMENT");
+	
+	if(token->type == EOL){
+		token = getToken();
+		return true;
+	}
+	else if(token->type == ID){
+		token = getToken();
+		if(token->type == ASSIGNMENT_EQ){
+			token = getToken();
+			if(CALL_ASSIGN()){
+				if(token->type == EOL){
+					token = getToken();
+					return true;
+				}
+			}
+		}
+	}
+	else if(token->type == PRINT){
+		//TODO
+	}
+	else if(token->type == INPUT){
+		token = getToken();
+		if(token->type == ID){
+			token = getToken();
+			if(token->type == EOL){
+				token = getToken();
+				return true;
+			}
+		}
+	}
+	else if(token->type == RETURN){
+		//TODO
+	}
+	else if(token->type == DO){
+		//TODO
+	}
+	else if(token->type == IF){
+		//TODO
+	}
 	return false;
 }
 
 bool VALUE(){
 	debug_p("enter VALUE");
-	return false;
+	//TODO
+	return true;
 }
 
 bool ELSE_STATEMENT(){
 	debug_p("enter ELSE_STATEMENT");
+	if(token->type == END){
+		token = getToken();
+		return true;
+	}
+	else if(token->type == ELSE){
+		token = getToken();
+		if(token->type == EOL){
+			return STAT_LIST();
+		}
+	}
 	return false;
 }
 
 bool STAT_LIST(){
 	debug_p("enter STAT_LIST");
+
+	if(	token->type == EOL	 ||	token->type == ID     || token->type == PRINT || 
+		token->type == INPUT || token->type == RETURN || token->type == DO	  ||
+		token->type == IF){
+		token = getToken();
+		return STATEMENT() && STAT_LIST();
+	}
+	else if(token->type == LOOP || token->type == ELSE || token->type == END){
+		token = getToken();
+		return true;
+	}
 	return false;
 }
 
 bool EXPRESSION(){
 	debug_p("enter EXPRESSION");
+	//TODO
 	return false;
 }
 
 bool CALL_ASSIGN(){
 	debug_p("enter CALL_ASSIGN");
+	//TODO
 	return false;
 }
 
 bool PARAM_VALUE(){
 	debug_p("enter PARAM_VALUE");
+	//TODO
 	return false;
 }
 
 bool NEXT_PARAM_VALUE(){
 	debug_p("enter NEXT_PARAM_VALUE");
+	//TODO
 	return false;
 }
 
 bool PARAM_LIST(){
 	debug_p("enter PARAM_LIST");
+	if(token->type == ID){
+		return PARAM() && NEXT_PARAM();
+	}
+	else if(token->type == RIGHT_R_BRACKET){
+		token = getToken();
+		return true;
+	}
 	return false;
 }
 
 bool NEXT_PARAM(){
 	debug_p("enter NEXT_PARAM");
+	if(token->type == COMMA){
+		token = getToken();
+		return PARAM() && NEXT_PARAM();
+	}
+	else if(token->type == RIGHT_R_BRACKET){
+		token = getToken();
+		return true;
+	}
 	return false;
 }
 
 bool PARAM(){
 	debug_p("enter PARAM");
+	if(token->type == ID){
+		token = getToken();
+		if(token->type == AS){
+			token = getToken();
+			return DATA_TYPE();
+		}
+	}
 	return false;
 }
 
 bool DATA_TYPE(){
 	debug_p("enter DATA_TYPE");
+	if(token->type == INTEGER){
+		token = getToken();
+		return true;
+	}
+	else if(token->type == DOUBLE){
+		token = getToken();
+		return true;
+	}
+	else if(token->type == STRING){
+		token = getToken();
+		return true;
+	}
 	return false;
 }
 
