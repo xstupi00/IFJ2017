24d157097c2f227324f4a8ea454e57e243c63352
xstupi00 <simondestupinsky@gmail.com>
2017-11-19 10:38:39 +0100

    little changes

--- main.c
+++ main.c
@@ -7,60 +7,9 @@
 #include "expr.h"
 #include "generate.h"
 int main(){
-    //FILE *fp = fopen("S.TXT","r");
-    //T_Token *token;
+
     initToken();
     init_global_symtables();
-
-    //int i=0;
-    //for(i=0; i<5; i++){
-    //    token = getToken();
-    //    printf("%d\t", token->type);
-    //    printf("%s\n", token->str->string);
-    //}
-    //token = getToken();
-    //expression();
-       /* printf("%d\t", token->type);
-       // printf("%s\n", token->str->string);
-        printf("%d\n", token_is_operator(token));
-    //ungetToken();   
-    token = getToken();
-        printf("%d\t", token->type);
-        //printf("%s\n", token->str->string);
-        printf("%d\n", token_is_operator(token));
-    token = getToken();
-        printf("%d\t", token->type);
-        //printf("%s\n", token->str->string);
-        printf("%d\n", token_is_operator(token));
-    free(token->str->string);
-    free(token->str);
-    free(token);*/
-    
-    /*for(int i = 0; i < 5; i++){
-        T_Token *tok = getToken();
-        printf("%d\t", tok->type);
-        printf("%s\n", tok->str);
-    }*/
-
-    /* T_Stack *s = (T_Stack *) malloc(sizeof(T_Stack));
-    S_Init(s);
-
-    for ( int i = 0; i < 20; i++) {
-        int *x = malloc(sizeof(int));
-        *x = i;
-        S_Push(s, x);
-    }
-
-    S_Print(s);
-    int j = 0;
-    while ( !S_Empty(s) ) {
-        S_Pop(s);
-        printf("%d\n",j++);
-    }
-
-    S_Push(s,&j);
-    int *f = (int *)S_Top(s);
-    printf("%d\n",*f); */
     
     /*list_init();
     expression(NULL, NULL);
@@ -105,6 +54,4 @@ int main(){
     printf("WRITE LF@CON\n");*/
     variable_t * str = create_var(998);
     printf("%s\n", str->data.str);
-
-
 }
