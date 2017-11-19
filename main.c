f5399d8943f994b0cc14c192db7ac58e54a36407
xlisci02 <spse1po@gmail.com>
2017-11-19 10:32:20 +0100

    added generate code list

--- main.c
+++ main.c
@@ -5,11 +5,13 @@
 #include "stack.h"
 #include "parser.h"
 #include "expr.h"
-
+#include "generate.h"
 int main(){
     //FILE *fp = fopen("S.TXT","r");
     //T_Token *token;
     initToken();
+    init_global_symtables();
+
     //int i=0;
     //for(i=0; i<5; i++){
     //    token = getToken();
@@ -59,6 +61,50 @@ int main(){
     S_Push(s,&j);
     int *f = (int *)S_Top(s);
     printf("%d\n",*f); */
-    parse();
+    
+    /*list_init();
+    expression(NULL, NULL);
+    printf(".IFJcode17\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@VAR\n");
+    
+    for(instruction_t * tmp = list->First; tmp!=NULL; tmp=tmp->next){
+        printf("%s",tmp->instr_name); 
+        if(tmp->op1){
+            switch (tmp->op1->data_type){
+                case INTEGER:printf("int@%d",tmp->op1->data.i);break;
+                case DOUBLE:printf("float@%g",tmp->op1->data.d);break;
+                case STRING:printf("string@");
+                            process_string(tmp->op1->data.str); break;
+                default: if(tmp->op1->data.str)printf("%s", tmp->op1->data.str);break;
+            }
+        }
+       
+        if(tmp->op2){
+            switch (tmp->op2->data_type){
+                case INTEGER:printf("int@%d",tmp->op2->data.i);break;
+                case DOUBLE:printf("float@%g",tmp->op2->data.d); break;
+                case STRING:printf("string@");
+                            process_string(tmp->op2->data.str); break;
+                default: if(tmp->op2->data.str)printf("%s", tmp->op2->data.str);break;
+               }
+        }
+        if(tmp->op3)
+            switch (tmp->op3->data_type){
+                case INTEGER:printf("int@%d",tmp->op3->data.i);break;
+                case DOUBLE:printf("float@%g",tmp->op3->data.d); break;
+                case STRING:printf("string@");
+                            process_string(tmp->op3->data.str); break;
+                default: if(tmp->op3->data.str)printf("%s", tmp->op3->data.str);break;
+
+        }
+        printf("\n");
+    }
+    printf("WRITE LF@CON\n");*/
+    variable_t * str = create_var(998);
+    printf("%s\n", str->data.str);
+
 
 }
